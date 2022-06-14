aa8aa8fa541492720c90585973a8b6fa795ae494
Johannes Pablo Rey <johannespablorey@gmail.com>
2022-06-14 13:34:18 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+//import Planets from './components/planets';
+import NewRoutes from './routes';
+
+function App() {
+  return (
+    <div>
+      <NewRoutes/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import PlanetsScreen from './screens/planets';
+import PlanetScreen from './screens/planet';
+import NotFoundScreen from './screens/notFound';
+
+const NewRoutes = () => (
+    <BrowserRouter>
+        <Routes>
+            <Route exact path='/' element={<PlanetsScreen/>} />
+            <Route exact path='/planet/:id' element={<PlanetScreen/>} />
+            <Route path='*' element={<NotFoundScreen/>}/>
+        </Routes>
+    </BrowserRouter>
+);
+
+export default NewRoutes;
